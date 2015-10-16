386111f0a1966ef4066beeb2b7059348114ad4b7
ravitapil <ravi.h@obelisksolutions.in>
2015-10-16 18:33:11 +0530

    Adds Model class for Blogger Messages

--- app/models/Blog.scala
+++ app/models/Blog.scala
@@ -2,9 +2,14 @@ package models
 
 import java.util.Date
 
-import reactivemongo.bson.BSONObjectID
-
 /**
  * Created by ravi on 8/10/15.
  */
-case class BlogMessages(_id: BSONObjectID, userName: String, message: String, _date: Date)
+case class BlogMessages(userName: String, message: String, _date: Date)
+
+object BlogMessages{
+  import play.api.libs.json.Json
+  import play.modules.reactivemongo.json.BSONFormats._
+
+  implicit val blogMessagesFormat = Json.format[BlogMessages]
+}
