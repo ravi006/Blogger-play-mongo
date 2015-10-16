7d984674e0159d6a79b671b054e25e22bf668128
ravitapil <ravi.h@obelisksolutions.in>
2015-10-16 18:32:13 +0530

    Adds Model class for Blogger User

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,8 +1,16 @@
 package models
 
-/**
- * Created by ravi on 9/10/15.
- */
-class User {
+import play.api.libs.json.Json
+import reactivemongo.bson.BSONObjectID
 
+/**
+  * Created by ravi on 16/10/15.
+  */
+case class User(_id: BSONObjectID, email: String, password: String, firstName: String, lastName: String)
+
+
+object User{
+  import play.modules.reactivemongo.json.BSONFormats._
+
+  implicit val userFormat = Json.format[User]
 }
